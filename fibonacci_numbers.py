c1718aa4adb1e11afa91d608464f2515fed2d346
Steven <42501398+Steven-MKN@users.noreply.github.com>
2019-08-01 15:43:08 +0200

    Update fibonacci_numbers.py

--- fibonacci_numbers.py
+++ fibonacci_numbers.py
@@ -1,3 +1,8 @@
+#Write a program that asks the user how many Fibonnaci numbers to generate and then generates them. 
+#Make sure to ask the user to enter the number of numbers in the sequence to generate.
+#(Hint: The Fibonnaci seqence is a sequence of numbers where the next number in the sequence is the sum 
+#of the previous two numbers in the sequence. The sequence looks like this: 1, 1, 2, 3, 5, 8, 13, …)
+
 def get_fibonacci(start: int, how_many: int):
     num_list = [start]
     num_list.append(start + 0)
