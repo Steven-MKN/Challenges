78e59609a09c3df325b062f4480b4ec9a8626b01
Steven <42501398+Steven-MKN@users.noreply.github.com>
2019-08-01 15:45:27 +0200

    Update prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -1,3 +1,8 @@
+#Ask the user for a number and determine whether the number is prime or not. 
+#(For those who have forgotten, a prime number is a number that has no divisors).
+
+#I adjusted this to check for prime numbers with a given range, just for fun!
+
 def is_prime(x):
     for i in range(2,x):
         if x%i == 0:
